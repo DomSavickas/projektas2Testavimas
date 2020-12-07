6cb179153c9ef7718e977c63e9a5fd8110e0b32d
Dominykas <dominykas.savickas@knf.stud.vu.lt>
2020-12-07 21:55:50 +0200

    Initial commit (Unit tests completed)

--- app/src/test/java/com/example/projektas2/ExampleUnitTest.java
+++ app/src/test/java/com/example/projektas2/ExampleUnitTest.java
@@ -10,8 +10,30 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    @Test(expected = NullPointerException.class)
+    public void Given_NullString_When_CharCounterIsCalled_Then_NullPointerExceptionIsThrown(){
+       CharWordCounter.CharCounter(null);
+    }
+    @Test(expected = NullPointerException.class)
+    public void Given_NullString_When_WordCounterIsCalled_Then_NullPointerExceptionIsThrown(){
+        CharWordCounter.WordCounter(null);
+    }
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void Given_SixCharString_When_CharCounterIsCalled_Then_ReturnSix(){
+        final String givenString = "testas";
+
+        final int expectedResult = 6;
+        final int actualResult = CharWordCounter.CharCounter(givenString);
+
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void Given_String_When_WordCounterIsCalled_Then_ReturnThree(){
+        final String givenString = "testas testas testas";
+
+        final int expectedResult = 3;
+        final int actualResult = CharWordCounter.WordCounter(givenString);
+
+        assertEquals(expectedResult, actualResult);
     }
 }
